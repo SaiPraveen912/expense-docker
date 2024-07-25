7fc41bfeac3931605acedf4d848acb3ec0d54172
SaiPraveen912 <saipraveen.immanni@gmail.com>
2024-07-25 08:49:49 +0530

    expense-docker MySQL Dockerfile install docker using shell script

--- install-docker.sh
+++ install-docker.sh
@@ -6,8 +6,8 @@ N="\e[0m"
 
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
-usermod -aG docker
-echo -e "$R Logout and login again $N"
+usermod -aG docker ec2-user
+echo -e "$R Logout and Login again $N"
